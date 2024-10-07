cd88cfc0fd13fcc00848e5f8a8982dc39f1be9d7
Mark Lawlor <mwlawlor@gmail.com>
2024-10-08 05:23:42 +1000

    Rename to withAnchor

--- packages/expo-router/src/link/Link.tsx
+++ packages/expo-router/src/link/Link.tsx
@@ -91,8 +91,8 @@ export interface LinkProps<T extends string | object>
   /** Should push the current route  */
   push?: boolean;
 
-  /** Should this route replace the initial screen */
-  initial?: boolean;
+  /** If this route loads a new navigator, should its initialRouteName also be loaded. Default: False */
+  withAnchor?: boolean;
 
   /** On web, this sets the HTML `class` directly. On native, this can be used with CSS interop tools like Nativewind. */
   className?: string;
@@ -191,7 +191,7 @@ function ExpoRouterLink(
     rel,
     target,
     download,
-    initial,
+    withAnchor,
     ...rest
   }: LinkProps<any>,
   ref: ForwardedRef<Text>
@@ -213,7 +213,12 @@ function ExpoRouterLink(
   if (push) event = 'PUSH';
   if (replace) event = 'REPLACE';
 
-  const props = useLinkToPathProps({ href: resolvedHref, event, relativeToDirectory, initial });
+  const props = useLinkToPathProps({
+    href: resolvedHref,
+    event,
+    relativeToDirectory,
+    withAnchor,
+  });
 
   const onPress = (e: MouseEvent<HTMLAnchorElement> | GestureResponderEvent) => {
     if ('onPress' in rest) {
